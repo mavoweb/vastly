73f3fe6ff70cf3790cf2e02d1a4c63d3cc467866
Lea Verou <lea@verou.me>
2023-11-01 15:22:34 -0400

    Add rudimentary website infrastructure

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# vASTly
+<header>
+
+# vᴀꜱᴛly
 
 Everything you need to support a custom expression language in your application.
-Includes a host of utilities for handling expression ASTs (such as those produced by [JSEP](https://ericsmekens.github.io/jsep/)).
+
+</header>
+
+<main>
+
+## What is this?
+
+vᴀꜱᴛly is a toolkit for handling expression ASTs (such as those produced by [JSEP](https://ericsmekens.github.io/jsep/)).
 These ASTs are a subset of ASTs produced by full-blown parsers like [Esprima](https://esprima.org/).
 
 Intended to be used in conjunction with [JSEP](https://ericsmekens.github.io/jsep/), but should work with any AST that conforms to the same structure.
 
 Extracted from [Mavo](https://mavo.io).
+
+</main>
---
+++ _build/.eleventy.cjs
@@ -0,0 +1,47 @@
+const markdownIt = require("markdown-it");
+const anchor = require('markdown-it-anchor')
+const pluginTOC = require('eleventy-plugin-toc');
+const { globSync } = require("glob");
+
+module.exports = eleventyConfig => {
+	let data = {
+		layout: "page.njk",
+		permalink: "{{ page.filePathStem | replace('README', '') | replace('index', '') }}/index.html",
+		tests: globSync("./test/*.js").map(path => path.replace(/^.+\/|\.m?js$/g, "")).filter(name => !name.startsWith("index")),
+	};
+
+	for (let p in data) {
+		eleventyConfig.addGlobalData(p, data[p]);
+	}
+
+	eleventyConfig.setDataDeepMerge(true);
+
+	eleventyConfig.setLibrary("md", markdownIt({
+			html: true,
+		})
+		.disable("code")
+		.use(anchor, {
+			permalink: anchor.permalink.headerLink()
+		})
+	);
+
+	eleventyConfig.addFilter(
+		"relative",
+		page => {
+			let path = page.url.replace(/[^/]+$/, "");
+			let ret = require("path").relative(path, "/");
+
+			return ret || ".";
+		}
+	);
+
+	eleventyConfig.addPlugin(pluginTOC);
+
+	return {
+		markdownTemplateEngine: "njk",
+		templateFormats: ["md", "njk"],
+		dir: {
+			output: "."
+		}
+	};
+};
