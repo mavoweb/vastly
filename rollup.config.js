d034324c40be1132c124446b9d3758b5d3850421
Adam Janicki <adamjanicki2@gmail.com>
2023-11-27 18:00:48 -0500

    get rid of babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,26 @@
-import { getBabelOutputPlugin as babel } from '@rollup/plugin-babel';
-
 const BASE_FILENAME = "vastly";
 const IIFE_NAME = "Vastly";
 
-function bundle(format) {
+function bundle(format, filenameAddition = "") {
 	let filename = BASE_FILENAME;
 
-	if (format !== "esm") {
-		filename += "." + format;
+	if (format !== "esm" && filenameAddition) {
+		filename += "." + filenameAddition;
 	}
-
-	const plugins = [
-		babel({
-			presets: ["@babel/preset-env"],
-			allowAllFormats: true,
-		})
-	];
 
 	return {
 		file: `src/${filename}.js`,
 		name: IIFE_NAME,
 		format,
-		sourcemap: format !== "esm",
-		plugins
+		sourcemap: format !== "esm"
 	};
 }
 
 export default {
 	input: "src/index.js",
 	output: [
-		bundle("iife"),
+		bundle("iife", "global"),
 		bundle("esm"),
-		bundle("cjs"),
+		bundle("cjs", "cjs"),
 	],
 };
