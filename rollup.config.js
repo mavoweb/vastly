7d5533f2bf5ea6fcf340bf9108eb54e58900c46b
Adam Janicki <45052692+adamjanicki2@users.noreply.github.com>
2023-11-28 11:06:37 -0500

    adds rollup (#10)

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const BASE_FILENAME = "vastly";
+const IIFE_NAME = "Vastly";
+
+function bundle(format, filenameAddition = "") {
+	let filename = BASE_FILENAME;
+
+	if (format !== "esm" && filenameAddition) {
+		filename += "." + filenameAddition;
+	}
+
+	return {
+		file: `dist/${filename}.js`,
+		name: IIFE_NAME,
+		format,
+		sourcemap: format !== "esm"
+	};
+}
+
+export default {
+	input: "src/index.js",
+	output: [
+		bundle("iife", "global"),
+		bundle("esm"),
+		bundle("cjs", "cjs"),
+	],
+};
