fde3d439676ca802394098ced2e11d8c7f5137dd
Lea Verou <lea@verou.me>
2023-12-05 17:28:45 -0500

    [parse] Update to work with new parents module, make parser an option, add docs

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,13 @@
-import setParents from "./setParents.js";
+import { setAll as setParents } from "./parents.js";
 
-export default function parse (str, parser = parse.defaultParser) {
+/**
+ * Parses an expression into an AST, sets parent references, and returns the AST
+ * @param {string} str
+ * @param {object} [options]
+ * @param {function} [options.parser] - A function that takes a string and returns an AST. Defaults to `parse.defaultParser`
+ * @returns {object}
+ */
+export default function parse (str, { parser = parse.defaultParser } = {}) {
 	if (!parser) {
 		throw new Error("No parser");
 	}
