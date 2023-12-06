97a1c1ed019b98fa95a86eca569161524d66da0b
Adam Janicki <adamjanicki2@gmail.com>
2023-12-05 19:51:59 -0500

    delete transformations from serialize

---
+++ test/serialize.js
@@ -0,0 +1,21 @@
+import jsep from "../node_modules/jsep/dist/jsep.min.js";
+import serialize from "../src/serialize.js";
+
+export default {
+	name: "serialize()",
+	run (expr) {
+		const ast = jsep(expr);
+		return serialize(ast);
+	},
+	// Simple tests, should be beefed up in the future
+	tests: [
+		{
+			args: ["1 + (foo * 2)"],
+			expect: "1 + foo * 2"
+		},
+		{
+			args: ["foo.bar.baz()"],
+			expect: "foo.bar.baz()"
+		}
+	]
+}
