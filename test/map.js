8eb6406ada653adb056bf4f14d6ca2c51345e487
Adam Janicki <adamjanicki2@gmail.com>
2023-12-08 11:37:47 -0500

    fix indentation

--- test/map.js
+++ test/map.js
@@ -15,9 +15,16 @@ export default {
 			args: ["foo + bar + baz", () => undefined],
 			expect: ["foo + bar + baz", "foo + bar + baz"]
 		},
-        {
-			args: ["foo.bar + foo.bar.baz", (node) => node.type === "Identifier" && node.name !== "foo" ? {...node, name: "foo"} : undefined],
+		{
+			args: [
+				"foo.bar + foo.bar.baz",
+				(node) => {
+					if (node.type === "Identifier" && node.name !== "foo") {
+						return {...node, name: "foo"};
+					}
+				}
+			],
 			expect: ["foo.bar + foo.bar.baz",  "foo.foo + foo.foo.foo"]
-		},
+		}
 	]
 }
