db9a01815819e3545adf3343a8f304eb80aa7ebc
Adam Janicki <adamjanicki2@gmail.com>
2023-12-15 13:58:04 -0500

    implement transform

---
+++ test/transform.js
@@ -0,0 +1,43 @@
+import jsep from "../node_modules/jsep/dist/jsep.min.js";
+import transform from "../src/transform.js";
+import serialize from "../src/serialize.js";
+
+export default {
+	name: "transform()",
+	run (str, ...args) {
+		const ast = jsep(str);
+		transform(ast, ...args);
+		return serialize(ast);
+	},
+	tests: [
+		{
+			args: ["foo + bar + baz", () => undefined],
+			expect: "foo + bar + baz"
+		},
+		{
+			args: [
+				"foo.bar + foo.bar.baz",
+				(node) => {
+					if (node.type === "Identifier" && node.name !== "foo") {
+						return {...node, name: "foo"};
+					}
+				}
+			],
+			expect: "foo.foo + foo.foo.foo"
+		},
+		// test object transformer
+		{
+			args: [
+				"foo.bar + foo.bar.baz",
+				{
+					Identifier: (node) => {
+						if (node.name !== "foo") {
+							return {...node, name: "foo"};
+						}
+					}
+				}
+			],
+			expect: "foo.foo + foo.foo.foo"
+		}
+	]
+}
