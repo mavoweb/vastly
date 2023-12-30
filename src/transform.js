d57751368c6a4382960aa6e67d7fed3fcfea320e
Adam Janicki <45052692+adamjanicki2@users.noreply.github.com>
2023-12-30 13:23:28 -0500

    option 1 for transform (#23)
    * option 1 for transform
    
    * fix transform
    
    * add descriptions to tests
    
    * restore first version
    
    * fix spacing
    
    * decouple parent and transform
    
    * return callback result

---
+++ src/transform.js
@@ -0,0 +1,40 @@
+import { properties as childProperties } from "./children.js";
+import { matches } from "./util.js";
+import * as parents from "./parents.js";
+
+/**
+ * Recursively execute a callback on this node and all its children.
+ * If the callback returns a non-undefined value, it will overwrite the node.
+ * This function will not modify the root node of the input AST.
+ *
+ * @param {object | object[]} node AST node or array of nodes
+ * @param {function(object, string, object?)} transformCallback
+ * @param {object} [o]
+ * @param {string | string[] | function} [o.only] Only walk nodes of this type
+ * @param {string | string[] | function} [o.except] Ignore walking nodes of these types
+ * @returns {object | object[]} The callback's return value on the root node(s) of the input AST, or the root node(s) if the callback did not return a value
+ */
+export default function transform (node, transformCallback, o) {
+	return _transform(node, transformCallback, o);
+}
+
+function _transform (node, callback, o = {}, property, parent) {
+	if (Array.isArray(node)) {
+		return node.map(n => _transform(n, callback, o, property, parent));
+	}
+
+	const ignore = o.except && matches(node, o.except);
+	const explore = !ignore && matches(node, o.only);
+
+	if (explore) {
+		const transformedNode = callback(node, property, parent);
+		node = transformedNode !== undefined ? transformedNode : node;
+		parents.set(node, parent, {force: true});
+		const properties = childProperties[node.type] ?? [];
+		for (const prop of properties) {
+			node[prop] = _transform(node[prop], callback, o, prop, node);
+		}
+	}
+
+	return node;
+}
---
+++ test/transform.js
@@ -0,0 +1,51 @@
+import jsep from "../node_modules/jsep/dist/jsep.min.js";
+import transform from "../src/transform.js";
+import serialize from "../src/serialize.js";
+
+export default {
+	name: "transform()",
+	run (str, ...args) {
+		const ast = jsep(str);
+		transform(ast, ...args);
+		return serialize(ast);
+	},
+	tests: [
+		{
+			args: ["foo + bar + baz", () => undefined],
+			expect: "foo + bar + baz",
+			description: "Empty transform"
+		},
+		{
+			args: [
+				"foo",
+				(node) => ({...node, name: "bar"}),
+			],
+			expect: `foo`,
+			description: "Does not modify root node"
+		},
+		{
+			args: [
+				"foo.bar + foo.bar.baz",
+				(node) => {
+					if (node.type === "Identifier" && node.name !== "foo") {
+						return {...node, name: "foo"};
+					}
+				}
+			],
+			expect: "foo.foo + foo.foo.foo",
+			description: "Rewrite tree of size > 1"
+		},
+		{
+			args: [
+				"foo + bar * baz",
+				(node) => {
+					if (node.type === "BinaryExpression" && node.operator === "*") {
+						return {name: "prod", type: "Identifier"};
+					}
+				}
+			],
+			expect: "foo + prod",
+			description: "Rewrite to different node type"
+		}
+	]
+};
