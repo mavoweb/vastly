4c962b89f0b4531f9402fb8e24c3c864bd5c7971
Adam Janicki <adamjanicki2@gmail.com>
2024-01-04 12:30:36 -0500

    object literal for `transform`

--- src/transform.js
+++ src/transform.js
@@ -8,14 +8,15 @@ import * as parents from "./parents.js";
  * This function will not modify the root node of the input AST.
  *
  * @param {object | object[]} node AST node or array of nodes
- * @param {function(object, string, object?)} transformCallback
+ * @param {Object.<string, function> | function(object, string, object?)} transformation A map of node types to callbacks, or a single callback that will be called for all node types
  * @param {object} [o]
  * @param {string | string[] | function} [o.only] Only walk nodes of this type
  * @param {string | string[] | function} [o.except] Ignore walking nodes of these types
  * @returns {object | object[]} The callback's return value on the root node(s) of the input AST, or the root node(s) if the callback did not return a value
  */
-export default function transform (node, transformCallback, o) {
-	return _transform(node, transformCallback, o);
+export default function transform (node, transformation, o) {
+	const callback = getTransformMapCallback(transformation);
+	return _transform(node, callback, o);
 }
 
 function _transform (node, callback, o = {}, property, parent) {
@@ -37,4 +38,18 @@ function _transform (node, callback, o = {}, property, parent) {
 	}
 
 	return node;
+}
+
+export function getTransformMapCallback (cb) {
+	if (typeof cb === "function") {
+		return cb;
+	}
+	else if (typeof cb === "object") {
+		return (node, property, parent) => {
+			if (cb[node.type]) {
+				return cb[node.type](node, property, parent);
+			}
+		};
+	}
+	return () => undefined;
 }
