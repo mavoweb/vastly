8e8bd9b51e8eb538f42fbda0ab735d07975c4259
Adam Janicki <adamjanicki2@gmail.com>
2024-01-05 13:00:04 -0500

    add array option to map

--- src/map.js
+++ src/map.js
@@ -5,18 +5,17 @@ import transform from "./transform.js";
  * If the callback returns a non-undefined value, it will overwrite the node,
  * otherwise it will return a shallow clone.
  * @param {object | object[]} node AST node or array of nodes
- * @param {function(object, string, object?)} callback
+ * @param {Object.<string, function> | function(object, string, object?) | (Object.<string, function> | function(object, string, object?))[]} mappings A map of node types to callbacks, or a single callback that will be called for all node types, or a list of either, which will be applied in order
  * @param {object} [o]
  * @param {string | string[] | function} [o.only] Only walk nodes of this type
  * @param {string | string[] | function} [o.except] Ignore walking nodes of these types
  */
-export default function map (node, callback, o) {
-	return transform(
-		node,
-		(node, property, parent) => {
-			const ret = callback(node, property, parent);
-			return ret !== undefined ? ret : {...node};
-		},
-		o
+export default function map (node, mappings, o) {
+	if (!Array.isArray(mappings)) {
+		mappings = [mappings];
+	}
+	mappings.push(
+		(node, property, parent, originalNode) => node === originalNode ? {...node} : node
 	);
+	return transform(node, mappings, o);
 }
