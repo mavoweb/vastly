c8e6b480e536b19cbc03dde880e821cfe5c795cf
Adam Janicki <45052692+adamjanicki2@users.noreply.github.com>
2024-01-04 20:59:55 -0500

    object literal for `transform` (#28)
    * object literal for `transform`
    
    * get rid of helper
    
    * fix lint

--- src/transform.js
+++ src/transform.js
@@ -8,31 +8,37 @@ import * as parents from "./parents.js";
  * This function will not modify the root node of the input AST.
  *
  * @param {object | object[]} node AST node or array of nodes
- * @param {function(object, string, object?)} transformCallback
+ * @param {Object.<string, function> | function(object, string, object?)} transformation A map of node types to callbacks, or a single callback that will be called for all node types
  * @param {object} [o]
  * @param {string | string[] | function} [o.only] Only walk nodes of this type
  * @param {string | string[] | function} [o.except] Ignore walking nodes of these types
- * @returns {object | object[]} The callback's return value on the root node(s) of the input AST, or the root node(s) if the callback did not return a value
+ * @returns {object | object[]} The transformation's return value on the root node(s) of the input AST, or the root node(s) if the transformation did not return a value
  */
-export default function transform (node, transformCallback, o) {
-	return _transform(node, transformCallback, o);
+export default function transform (node, transformation, o) {
+	return _transform(node, transformation, o);
 }
 
-function _transform (node, callback, o = {}, property, parent) {
+function _transform (node, transformation, o = {}, property, parent) {
 	if (Array.isArray(node)) {
-		return node.map(n => _transform(n, callback, o, property, parent));
+		return node.map(n => _transform(n, transformation, o, property, parent));
 	}
 
 	const ignore = o.except && matches(node, o.except);
 	const explore = !ignore && matches(node, o.only);
 
 	if (explore) {
-		const transformedNode = callback(node, property, parent);
+		let transformedNode;
+		if (typeof transformation === "function") {
+			transformedNode = transformation(node, property, parent);
+		}
+		else if (typeof transformation === "object") {
+			transformedNode = transformation[node.type]?.(node, property, parent);
+		}
 		node = transformedNode !== undefined ? transformedNode : node;
 		parents.set(node, parent, {force: true});
 		const properties = childProperties[node.type] ?? [];
 		for (const prop of properties) {
-			node[prop] = _transform(node[prop], callback, o, prop, node);
+			node[prop] = _transform(node[prop], transformation, o, prop, node);
 		}
 	}
 
