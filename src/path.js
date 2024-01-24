65b9dac0916d81f890e35e996fdd6fdaffba8897
Adam Janicki <adamjanicki2@gmail.com>
2024-01-24 13:38:09 -0500

    with parents

--- src/path.js
+++ src/path.js
@@ -1,4 +1,5 @@
 import { properties } from "./children.js";
+import * as parents from "./parents.js";
 
 /**
  * Find a path from an ancestor node to a descendant node.
@@ -7,6 +8,13 @@ import { properties } from "./children.js";
  * @returns {(string | number)[] | null} an array of keys to traverse to get from the ancestor to the descendant, or null if no path exists
  */
 export default function path (ancestor, descendant) {
+	if (parents.get(descendant)) {
+		return parentPath(ancestor, descendant);
+	}
+	return defaultPath(ancestor, descendant);
+}
+
+function defaultPath (ancestor, descendant) {
 	// Found descendant
 	if (ancestor === descendant) {
 		return [];
@@ -15,7 +23,7 @@ export default function path (ancestor, descendant) {
 	// Case where ancestor is an array for an array expression or compound etc
 	if (Array.isArray(ancestor)) {
 		for (let i = 0; i < ancestor.length; i++) {
-			const childPath = path(ancestor[i], descendant);
+			const childPath = defaultPath(ancestor[i], descendant);
 			if (childPath) {
 				return [i].concat(childPath);
 			}
@@ -26,7 +34,7 @@ export default function path (ancestor, descendant) {
 	const childProperties = properties[ancestor.type] ?? [];
 
 	for (const property of childProperties) {
-		const childPath = path(ancestor[property], descendant);
+		const childPath = defaultPath(ancestor[property], descendant);
 		if (childPath) {
 			return [property].concat(childPath);
 		}
@@ -35,3 +43,31 @@ export default function path (ancestor, descendant) {
 	return null;
 }
 
+function parentPath (ancestor, descendant) {
+	// walks up parent pointers until it finds the ancestor
+	let path = [];
+	let current = descendant;
+	while (current !== ancestor) {
+		const parent = parents.get(current);
+		if (!parent) {
+			return null;
+		}
+		const childProperties = properties[parent.type];
+		for (const property of childProperties) {
+			if (Array.isArray(parent[property])) {
+				const index = parent[property].indexOf(current);
+				if (index !== -1) {
+					path = [property, index].concat(path);
+					break;
+				}
+			}
+			else if (parent[property] === current) {
+				path.unshift(property);
+				break;
+			}
+		}
+		current = parent;
+	}
+
+	return path;
+}
--- test/path.js
+++ test/path.js
@@ -1,7 +1,11 @@
 import jsep from "../node_modules/jsep/dist/jsep.min.js";
 import path from "../src/path.js";
+import {setAll} from "../src/parents.js";
 
-const ast = jsep("1 + foo(bar, 1 + baz * 2)");
+const expr = "1 + foo(bar, 1 + baz * 2)";
+const ast = jsep(expr);
+const astWithParents = jsep(expr)
+setAll(astWithParents);
 
 export default {
 	name: "path()",
@@ -10,34 +14,39 @@ export default {
 	},
 	tests: [
 		{
+			name: "Same node",
 			args: [ast, ast],
-			expect: [],
-			description: "Same node"
+			expect: []
 		},
 		{
+			name: "Path length 1",
 			args: [ast, ast.right],
-			expect: ["right"],
-			description: "Path length 1"
+			expect: ["right"]
 		},
 		{
+			name: "Root to leaf",
 			args: [ast, ast.right.arguments[1].right.left],
-			expect: ["right", "arguments", 1, "right", "left"],
-			description: "Root to leaf"
+			expect: ["right", "arguments", 1, "right", "left"]
 		},
 		{
+			name: "Non-root to non-leaf",
 			args: [ast.right, ast.right.arguments[1]],
-			expect: ["arguments", 1],
-			description: "Non-root to non-leaf"
+			expect: ["arguments", 1]
 		},
 		{
+			name: "Non-root to leaf",
 			args: [ast.right, ast.right.arguments[0]],
-			expect: ["arguments", 0],
-			description: "Non-root to leaf"
+			expect: ["arguments", 0]
 		},
 		{
+			name: "Non-existent path",
 			args: [ast, jsep("dne")],
-			expect: null,
-			description: "Non-existent path"
-		}
+			expect: null
+		},
+		{
+			name: "Root to leaf with parents",
+			args: [astWithParents, astWithParents.right.arguments[1].right.left],
+			expect: ["right", "arguments", 1, "right", "left"]
+		},
 	]
 };
