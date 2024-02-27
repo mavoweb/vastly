fb87b2b3156c58697940765893fa15a6cc8d1eab
Adam Janicki <adamjanicki2@gmail.com>
2024-02-27 08:55:10 -0500

    add comments

--- src/prepend.js
+++ src/prepend.js
@@ -8,47 +8,45 @@ import * as children from "./children.js";
  * @param {*} [o]
  * @param {boolean} [o.computed] Whether to use computed syntax (e.g. `foo[bar]`) or dot syntax (e.g. `foo.bar`)
  */
-export default function prepend (node, prependee, o = {}) {
+export default function prepend(node, prependee, o = {}) {
 	const prependedNode = _prepend(node, prependee, o);
 	children.replace(node, prependedNode);
-	parents.update(prependedNode, {force: true});
+	parents.update(prependedNode, { force: true });
 	return prependedNode;
 }
 
-
-
-function _prepend (node, prependee, o = {}) {
+function _prepend(node, prependee, o = {}) {
 	const computed = o.computed || !isValidDotSyntax(node);
 	let prependedNode;
-	// complex case for CallExpressions
+	// if the node is a CallExpression, we need to prepend to the callee
 	if (node.type === "CallExpression" && !computed) {
-		const {callee, arguments: args} = node;
+		const { callee, arguments: args } = node;
 		prependedNode = {
 			type: "CallExpression",
 			arguments: args,
 			callee: _prepend(callee, prependee),
 		};
 	}
+	// if the node is a MemberExpression, we need to prepend to the object of the MemberExpression
 	else if (node.type === "MemberExpression" && !computed) {
-		// if the property is a MemberExpression, we need to prepend the object to the object of the MemberExpression
 		prependedNode = {
 			...node,
-			object: _prepend(node.object, prependee)
+			object: _prepend(node.object, prependee),
 		};
-	}
-	else {
+	} else {
 		prependedNode = {
 			type: "MemberExpression",
 			computed,
 			object: prependee,
-			property: node
+			property: node,
 		};
 	}
 
 	return prependedNode;
 }
 
-function isValidDotSyntax (node) {
+// checks if a node is a valid candidate for dot syntax
+function isValidDotSyntax(node) {
 	const type = node.type;
 	if (["Identifier", "ThisExpression", "CallExpression"].includes(type)) {
 		return true;
